d39df48c2a490cb8b80924ca2d381b5230cb9d3c
Benjamin Cooke Williams <83699317+bcookew@users.noreply.github.com>
2022-05-02 13:47:32 -0400

    console.table example

---
+++ Bits/consoleTable.js
@@ -0,0 +1,10 @@
+const people = [
+    {name: "Mike", age: 21},
+    {name: "Ruben", age: 21},
+    {name: "Antoine", age: 21},
+    {name: "Nichole", age: 21},
+    {name: "Saurabh", age: 21},
+    
+];
+
+console.table(people)
