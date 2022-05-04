071d1d7e89f0446edcb30981b6721923421c4344
Benjamin Cooke Williams <83699317+bcookew@users.noreply.github.com>
2022-05-04 17:23:42 -0400

    Added useState and age increment

--- React/ClassComponents/prop-it-up-functional/src/Components/PersonCard.js
+++ React/ClassComponents/prop-it-up-functional/src/Components/PersonCard.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const PersonCard = props => {
-    const { firstName, lastName, hairColor, age } = props;
+    const { firstName, lastName, hairColor } = props;
+    const [state, setState] = useState({
+        age: props.age
+    });
+    const increaseAge = () => {
+        setState({
+            age: state.age + 1
+        });
+    }
     return (
         <div>
             <h1>{lastName}, {firstName}</h1>
-            <p>Age: {age}</p>
+            <p>Age: {state.age}</p>
             <p>Hair Color: {hairColor}</p>
+            <button onClick={increaseAge}>Birthday Time</button>
         </div>
     );
 }
